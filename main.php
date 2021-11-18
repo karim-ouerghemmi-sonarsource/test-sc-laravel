318cae90750c9fabe84f9f61a281a1d5ea6ae1e8
Karim El Ouerghemmi <karim.ouerghemmi@sonarsource.com>
2021-11-18 11:12:21 +0100

    modify

--- main.php
+++ main.php
@@ -3,10 +3,3 @@
 Route::get('test-secu/{evil_variable}', function ($evil_variable) {
     echo print_r(DB::table('users')->whereRaw('first_name = ' . $evil_variable . "'"), true);
 });
-
-
-$join->on('users.id', '=', 'role_user.user_id')
-    ->whereRaw("role_user.active = true and role_user.instance_id = " . InstanceHelper::getInstanceId()
-    . " AND role_user.role_id = '" . $_GET[1] . "'");
-
-echo $_GET[1];
